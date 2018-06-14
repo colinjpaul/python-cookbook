f9d2a39ffa20e328d0e7687505da892b6f1e467b
Colin <Colin_Paul@McAfee.com>
2018-06-14 13:27:02 +0100

    dictionary comp

--- sandbox/dc_example_1.py
+++ sandbox/dc_example_1.py
@@ -1,3 +1,19 @@
-dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
-# Put all keys of `dict1` in a list and returns the list
-dict1.keys()
+dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+
+double_dict1 = {k:v*2 for (k,v) in dict1.items()}
+
+# Solving problems using dictionary comprehension
+
+# Using a For loop
+numbers = range(10)
+new_dict_for = {}
+
+for n in numbers:
+    if n % 2 == 0:
+        new_dict_for[n] = n ** 2
+
+# Using Dictionary Comprehsnsive
+new_dict_comp = {n:n**2 for n in numbers if n%2 == 0}
+
+print(new_dict_for)
+print(new_dict_comp)
