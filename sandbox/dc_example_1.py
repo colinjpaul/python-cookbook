037c4e78921e6d31095ee28c52ca6cb4ce3e8cf7
Colin <Colin_Paul@McAfee.com>
2018-06-15 09:00:51 +0100

    morning python

--- sandbox/dc_example_1.py
+++ sandbox/dc_example_1.py
@@ -12,8 +12,21 @@ for n in numbers:
     if n % 2 == 0:
         new_dict_for[n] = n ** 2
 
-# Using Dictionary Comprehsnsive
+# Using Dictionary Comprehension
 new_dict_comp = {n:n**2 for n in numbers if n%2 == 0}
 
-print(new_dict_for)
-print(new_dict_comp)
+# print(new_dict_for)
+# print(new_dict_comp)
+
+# Lambda functions
+# Initialize `fahrenheit` dictionary
+fahrenheit = {'t1':-30, 't2':-20, 't3':-10, 't4':0}
+
+# Get the corresponding 'celcius' values
+celsius = list(map(lambda x: (float(5)/9)*(x-32), fahrenheit.values()))
+
+#Create the `celsius` dictionary
+celsius_dict = dict(zip(fahrenheit.keys(), celsius))
+
+print(celsius)
+print(celsius_dict)
